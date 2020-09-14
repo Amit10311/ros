c588a64ce4ec703167ddd7d711d669548936ca67
Amit Kumar <amitkumar10311@gmail.com>
2020-09-14 11:00:46 +0200

    Add files via upload

---
+++ ros_basic_c++/classes/src/jedi_class.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Jedi {
+public:
+  string name;
+
+  Jedi(string n) { name = n; }
+
+  void say_hi() { cout << "Hello, my name is " << name << "\n"; }
+};
+
+int main(int argc, char **argv) {
+  Jedi j("ObiWan");
+  j.say_hi();
+  return 0;
+}
